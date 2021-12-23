c4909d67523e8e3d16bf5e9d39fb4c30ed8ae8bd
irrelevantRyan <ryanz22@nycstudents.net>
2021-12-23 10:34:39 -0500

    File structure of app

---
+++ apps/ai.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+def app():
+    st.title("AI stuff")
+
+    st.write("do some ai stuff")
+
+    st.write("if you are doing this part you are a god and you are really just too smart so you dont even need to care about coding cause you are literally machine learning engineer pls teach me.")
---
+++ apps/data.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+def app():
+    st.title("DATA Collections")
+
+    st.write("So you would need to do some citizen scientist stuff so whoever dev this part of the website should do the data collection")
+
+    st.write("yous hould explore streamlit components to learn what is possible")
